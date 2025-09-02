f1c7e81a06caaf996fa50f7fb52e8bfb76e602e7
TaixenFang <2491517@edu.vaniercollege.qc.ca>
2025-09-02 13:22:04 -0400

    Add files via upload

---
+++ module2.py
@@ -0,0 +1,15 @@
+def my_function():
+    print("Hello, World!")
+
+my_function()
+
+def input_output():
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    height = float(input("Enter your height in meters: "))
+    
+    print(f"Hello, (name)!")
+    print(f"You are (age) years old.")
+    print(f"Your height is (height) meters.")
+
+input_output()
